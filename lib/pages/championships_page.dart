e3b6e38156a9d29e98329db8eb9d2bdc2afc23d9
Vinícius Mioto <60669449+viniciusmioto@users.noreply.github.com>
2020-06-19 20:38:39 -0300

    lorem-ipsum

--- lib/pages/championships_page.dart
+++ lib/pages/championships_page.dart
@@ -12,6 +12,40 @@ class _ChampionshipsPageState extends State<ChampionshipsPage> {
       appBar: AppBar(
         title: Text("COMPETIÇÕES"),
       ),
+      body: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (context, index) {
+            return Card(
+              color: index % 2 == 0 ? Colors.blue : null,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'COMPETIÇÃO',
+                      style: TextStyle(
+                          color: index % 2 == 0 ? Colors.white : Colors.blue,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Os treinos de sexta começaram 10 min mais cedo, ou seja, às 13h20. ' +
+                          'Além disso, sábado será realizado um amistoso, ao invés do treino convencional.' +
+                          'Todos estão convidados a comparecer',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.justify,
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
--- lib/pages/news-page.dart
+++ lib/pages/news-page.dart
@@ -11,9 +11,45 @@ class _NewsPageState extends State<NewsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
         title: Text("IFPR SPORTS"),
       ),
+      drawer: Drawer(),
+      body: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'AVISO!',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Os treinos de sexta começaram 10 min mais cedo, ou seja, às 13h20. ' +
+                          'Além disso, sábado será realizado um amistoso, ao invés do treino convencional.' +
+                          'Todos estão convidados a comparecer',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.justify,
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
