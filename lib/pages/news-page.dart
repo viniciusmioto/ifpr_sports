c42801d8f41e512a0296396d86b5b6086ee0c166
Vinícius Mioto <60669449+viniciusmioto@users.noreply.github.com>
2020-06-20 14:07:27 -0300

    news-page

--- lib/pages/news-page.dart
+++ lib/pages/news-page.dart
@@ -15,52 +15,85 @@ class _NewsPageState extends State<NewsPage>
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
-        title: Text("IFPR SPORTS"),
+        title: Text('SPORTIF'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: Icon(Icons.search),
+          ),
+        ],
       ),
       drawer: Drawer(),
-      body: ListView.builder(
-          itemCount: 40,
-          itemBuilder: (context, index) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 60,
-                      child: Image.asset("assets/apito.png"),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'AVISO! ${index + 1}',
-                      style: TextStyle(
-                          color: Colors.green,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Os treinos de sexta começaram 10 min mais cedo, ou seja, às 13h20. ' +
-                          'Além disso, sábado será realizado um amistoso, ao invés do treino convencional.' +
-                          'Todos estão convidados a comparecer',
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                      textAlign: TextAlign.justify,
-                    )
-                  ],
-                ),
-              ),
-            );
-          }),
+      body: Container(
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+            cardItem(),
+          ],
+        ),
+      ),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
+}
+
+Widget cardItem() {
+  return Card(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        const ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.pexels.com/photos/1543924/pexels-photo-1543924.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"),
+          ),
+          title: Text('Prof. João Silva'),
+          subtitle: Text("09/05/2019 18:37"),
+          trailing: Icon(Icons.more_vert),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            child: Text(
+              'Atenção ATLETAS!\n' +
+                  'Reforçando o aviso dos horários de treinos dessa semana!\n' +
+                  'Os treinos de sexta serão realizados 10 minutos mais cedo, ou seja, o novo horário é 13:20',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ),
+        ButtonTheme(
+          child: ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                child: Icon(Icons.star),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Icon(Icons.favorite),
+                onPressed: () {
+                },
+              )
+
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
--- lib/pages/training-page.dart
+++ lib/pages/training-page.dart
@@ -17,44 +17,6 @@ class _TrainingPageState extends State<TrainingPage>
           Icons.access_time,
         ),
       ),
-      body: ListView.builder(
-          itemCount: 9,
-          itemBuilder: (context, index) {
-            return Card(
-              color: index % 2 == 0 ? Colors.green : null,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 60,
-                      child: Image.asset("assets/futsal.png"),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'FUTSAL',
-                      style: TextStyle(
-                          color: index % 2 == 0 ? Colors.white : Colors.green,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      'Os treinos de sexta começaram 10 min mais cedo, ou seja, às 13h20. ' +
-                          'Além disso, sábado será realizado um amistoso, ao invés do treino convencional.' +
-                          'Todos estão convidados a comparecer',
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                      textAlign: TextAlign.justify,
-                    )
-                  ],
-                ),
-              ),
-            );
-          }),
     );
   }
 
