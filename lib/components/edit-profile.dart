7ccac8d6606f716a1e59eecab1d5430b46f06149
Vinícius Mioto <60669449+viniciusmioto@users.noreply.github.com>
2020-07-02 12:10:33 -0300

    stars

--- lib/components/edit-profile.dart
+++ lib/components/edit-profile.dart
@@ -14,102 +14,139 @@ class _EditProfileForm extends State<EditProfileForm> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        SizedBox(
-          height: 10,
-        ),
-        Form(
-          key: _fornKey,
-          child: Column(
-            children: [
-              TextFormField(
-                autofocus: false,
-                onChanged: (value) {
-                  setState(() => _currentName = value);
-                },
-                validator: (value) => value.isEmpty ? 'Insira um nome' : null,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: "NOME",
-                  labelStyle: TextStyle(
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Form(
+            key: _fornKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  autofocus: false,
+                  onChanged: (value) {
+                    setState(() => _currentName = value);
+                  },
+                  validator: (value) => value.isEmpty ? 'Insira um nome' : null,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: "NOME",
+                    labelStyle: TextStyle(
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                  ),
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                validator: (value) => value.isEmpty ? 'Insira um email' : null,
-                autofocus: false,
-                onChanged: (value) {
-                  setState(() => _currentEmail = value);
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: "EMAIL",
-                  labelStyle: TextStyle(
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
+                SizedBox(height: 10),
+                TextFormField(
+                  validator: (value) =>
+                      value.isEmpty ? 'Insira um email' : null,
+                  autofocus: false,
+                  onChanged: (value) {
+                    setState(() => _currentEmail = value);
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "EMAIL",
+                    labelStyle: TextStyle(
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                  ),
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                autofocus: false,
-                validator: (value) =>
-                    value.isEmpty ? 'Insira uma modalidade' : null,
-                onChanged: (value) {
-                  setState(() => _currentModalidade = value);
-                },
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: "MODALIDADES",
-                  labelStyle: TextStyle(
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
+                SizedBox(height: 10),
+                TextFormField(
+                  autofocus: false,
+                  validator: (value) =>
+                      value.isEmpty ? 'Insira uma modalidade' : null,
+                  onChanged: (value) {
+                    setState(() => _currentModalidade = value);
+                  },
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: "MODALIDADES",
+                    labelStyle: TextStyle(
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                  ),
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                autofocus: false,
-                onChanged: (value) {
-                  setState(() => _currentAvatarUrl = value);
-                },
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  labelText: "AVATAR URL",
-                  labelStyle: TextStyle(
-                    color: Colors.black45,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
+                SizedBox(height: 10),
+                TextFormField(
+                  autofocus: false,
+                  onChanged: (value) {
+                    setState(() => _currentAvatarUrl = value);
+                  },
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: "AVATAR URL",
+                    labelStyle: TextStyle(
+                      color: Colors.black45,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                  ),
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ],
+                SizedBox(height: 20),
+              ],
+            ),
           ),
-        ),
-        FlatButton(
-          child: Icon(Icons.save),
-          onPressed: () {},
-        ),
-      ],
+           Container(
+                height: 40,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.bottomRight,
+                    stops: [0.3, 1],
+                    colors: [
+                      Colors.green,
+                      Colors.greenAccent,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(7),
+                  ),
+                ),
+                child: SizedBox.expand(
+                  child: FlatButton(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "SALVAR",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Icon(
+                          Icons.save,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                    onPressed: () {
+                    },
+                  ),
+                ),
+              ),
+        ],
+      ),
     );
   }
 }
--- lib/components/post-messages.dart
+++ lib/components/post-messages.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PostMessages extends StatelessWidget {
+class PostMessages extends StatefulWidget {
   final String urlImage;
   final String user;
   final String time;
@@ -9,24 +9,47 @@ class PostMessages extends StatelessWidget {
   PostMessages(this.urlImage, this.user, this.time, this.message);
 
   @override
+  _PostMessagesState createState() => _PostMessagesState();
+}
+
+class _PostMessagesState extends State<PostMessages> {
+  bool _star = false;
+  Color cor = Colors.grey;
+
+  @override
   Widget build(BuildContext context) {
+    void _switchStar() {
+      if (_star == true) {
+        _star = false;
+      } else {
+        _star = true;
+      }
+    }
+
+    void _changeColor() {
+      setState(() {
+        _switchStar();
+        cor = _star ? Colors.green : Colors.grey;
+      });
+    }
+
     return Card(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           ListTile(
             leading: CircleAvatar(
-              backgroundImage: NetworkImage(urlImage),
+              backgroundImage: NetworkImage(widget.urlImage),
             ),
-            title: Text(user),
-            subtitle: Text(time),
+            title: Text(widget.user),
+            subtitle: Text(widget.time),
             trailing: Icon(Icons.more_vert),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
               child: Text(
-                message,
+                widget.message,
                 style: TextStyle(
                   fontSize: 16,
                 ),
@@ -37,12 +60,14 @@ class PostMessages extends StatelessWidget {
             buttonColor: Colors.greenAccent,
             child: ButtonBar(
               children: <Widget>[
-                FlatButton(
-                  child: Icon(
+                IconButton(
+                  icon: Icon(
                     Icons.star,
-                    color: Colors.greenAccent,
+                    color: cor,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    _changeColor();
+                  },
                 )
               ],
             ),
