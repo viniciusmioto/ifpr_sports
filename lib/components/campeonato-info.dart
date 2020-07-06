e007b76971b916f139e79c1c241a0f7fe342c2b8
Vinícius Mioto <60669449+viniciusmioto@users.noreply.github.com>
2020-07-06 18:49:44 -0300

    champions-button

--- lib/components/campeonato-info.dart
+++ lib/components/campeonato-info.dart
@@ -16,66 +16,76 @@ class CampeonatoInfo extends StatelessWidget {
     return Container(
       child: Card(
         color: cor,
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: SizedBox(
-                height: 80,
-                width: 80,
-                child: Image.asset(imagem),
+        child: FlatButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              CupertinoPageRoute(
+                builder: (context) => TeamPage(),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                child: Text(
-                  competicao,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
+            );
+          },
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SizedBox(
+                  height: 80,
+                  width: 80,
+                  child: Image.asset(imagem),
                 ),
               ),
-            ),
-            Divider(
-              height: 20,
-              thickness: 5,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                '\nDATA: ' + data + '\nLOCAL: ' + local,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            ButtonTheme(
-              buttonColor: Colors.greenAccent,
-              child: ButtonBar(
-                children: <Widget>[
-                  FlatButton(
-                    child: Icon(
-                      Icons.people,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  child: Text(
+                    competicao,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                       color: Colors.white,
                     ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        CupertinoPageRoute(
-                          builder: (context) => TeamPage(),
-                        ),
-                      );
-                    },
-                  )
-                ],
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
-            ),
-          ],
+              Divider(
+                height: 20,
+                thickness: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '\nDATA: ' + data + '\nLOCAL: ' + local,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              ButtonTheme(
+                buttonColor: Colors.greenAccent,
+                child: ButtonBar(
+                  children: <Widget>[
+                    FlatButton(
+                      child: Icon(
+                        Icons.people,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                            builder: (context) => TeamPage(),
+                          ),
+                        );
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
