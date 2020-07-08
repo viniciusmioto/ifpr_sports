586eeb14622e5347ae8ce97741ba9a7e0dae2465
Vinícius Mioto <60669449+viniciusmioto@users.noreply.github.com>
2020-07-08 11:15:43 -0300

    shared-preferences

--- lib/provider/atleta-provider.dart
+++ lib/provider/atleta-provider.dart
@@ -1,29 +1,26 @@
 import 'dart:convert';
-import 'dart:math';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:ifsports/classes/atleta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AtletasProvider with ChangeNotifier {
-  Map<String, Atleta> _items = {};
+  var _athletas = new List<Atleta>();
+  SharedPreferences _prefs;
+
+  AtletasProvider() {
+    _loadFromPrefs();
+  }
 
   Future<List<Atleta>> get all async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    var data = prefs.getString('data');
-    if (data != null) {
-      Iterable decoded = jsonDecode(data);
-      List<Atleta> result = decoded.map((x) => Atleta.fromJson(x)).toList();
-      return result;
-    }
-    return null;
+    return [..._athletas];
   }
 
   int get count {
-    return _items.length;
+    return _athletas.length;
   }
 
   Atleta byIndex(int index) {
-    return _items.values.elementAt(index);
+    return _athletas.elementAt(index);
   }
 
   void put(Atleta atleta) {
@@ -31,44 +28,47 @@ class AtletasProvider with ChangeNotifier {
       return;
     }
 
-    if (atleta.id != null &&
-        atleta.id.trim().isNotEmpty &&
-        _items.containsKey(atleta.id)) {
-      _items.update(
-        atleta.id,
-        (_) => Atleta(
+    if (atleta.id != null && atleta.id.trim().isNotEmpty) {
+      _athletas.add(
+        Atleta(
           id: atleta.id,
           name: atleta.name,
           modalidade: atleta.modalidade,
           avatarUrl: atleta.avatarUrl,
         ),
       );
-    } else {
-      final id = Random().nextDouble().toString();
-      _items.putIfAbsent(
-        id,
-        () => Atleta(
-          id: id,
-          name: atleta.name,
-          modalidade: atleta.modalidade,
-          avatarUrl: atleta.avatarUrl,
-        ),
-      );
     }
-    save();
+    _saveToPrefs();
+    _loadFromPrefs();
     notifyListeners();
   }
 
   void remove(Atleta atleta) {
-    if (atleta != null && atleta.id != null) {
-      _items.remove(atleta.id);
-      save();
+    if (atleta.id != null) {
+      _athletas.remove(atleta);
+      _saveToPrefs();
+      _loadFromPrefs();
       notifyListeners();
     }
   }
 
-  save() async {
-    var prefs = await SharedPreferences.getInstance();
-    await prefs.setString('data', jsonEncode(_items));
+  _initPrefs() async {
+    if (_prefs == null) {
+      _prefs = await SharedPreferences.getInstance();
+    }
+  }
+
+  Future _loadFromPrefs() async {
+    await _initPrefs();
+    var data = _prefs.getString('data');
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Atleta> result = decoded.map((x) => Atleta.fromJson(x)).toList();
+      _athletas = result;
+    }
+  }
+
+  Future _saveToPrefs() async {
+    await _prefs.setString('data', jsonEncode(_athletas));
   }
 }
