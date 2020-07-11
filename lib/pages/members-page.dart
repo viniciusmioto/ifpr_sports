c46336fad9cde227b1a0609931a54ea6fc88b4ec
Vinícius Mioto <60669449+viniciusmioto@users.noreply.github.com>
2020-07-10 21:39:26 -0300

    filter

--- lib/pages/members-page.dart
+++ lib/pages/members-page.dart
@@ -4,7 +4,13 @@ import 'package:ifsports/components/members-list.dart';
 import 'package:ifsports/services/database.dart';
 import 'package:provider/provider.dart';
 
-class MembersPage extends StatelessWidget {
+class MembersPage extends StatefulWidget {
+  @override
+  _MembersPageState createState() => _MembersPageState();
+}
+
+class _MembersPageState extends State<MembersPage> {
+  bool _search = false;
   @override
   Widget build(BuildContext context) {
     return StreamProvider<List<Member>>.value(
@@ -12,11 +18,28 @@ class MembersPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           centerTitle: false,
-          title: Text('Membros'),
+          title: _search
+              ? TextFormField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: 'Buscar',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                  ),
+                  onChanged: (value) {},
+                )
+              : Text('Membros'),
           actions: [
             IconButton(
               icon: Icon(Icons.search),
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _search = !_search;
+                });
+              },
             ),
           ],
         ),
--- lib/pages/team-page.dart
+++ lib/pages/team-page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ifsports/components/atleta-tile.dart';
@@ -5,26 +7,62 @@ import 'package:ifsports/pages/members-page.dart';
 import 'package:ifsports/provider/atleta-provider.dart';
 import 'package:provider/provider.dart';
 
-class TeamPage extends StatelessWidget {
+class TeamPage extends StatefulWidget {
+  @override
+  _TeamPageState createState() => _TeamPageState();
+}
+
+class _TeamPageState extends State<TeamPage> {
+  bool _search = false;
+  String filter = '';
+  final _debouncer = Debouncer(milliseconds: 600);
   @override
   Widget build(BuildContext context) {
     final AtletasProvider users = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        title: Text('Time'),
+        title: _search
+            ? TextFormField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: 'Buscar',
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18,
+                  ),
+                ),
+                onChanged: (value) {
+                  _debouncer.run(() {
+                    setState(() {
+                      filter = value;
+                    });
+                  });
+                },
+              )
+            : Text('Time'),
         actions: [
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              _debouncer.run(() {
+                setState(() {
+                  _search = !_search;
+                });
+              });
+            },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: users.count,
-        itemBuilder: (ctx, index) => AtletaTile(users.byIndex(index)),
-      ),
+      body: _search
+          ? ListView.builder(
+              itemCount: 1,
+              itemBuilder: (ctx, index) =>
+                  AtletaTile(users.byIndexFilter(index, filter)))
+          : ListView.builder(
+              itemCount: users.count,
+              itemBuilder: (ctx, index) => AtletaTile(users.byIndex(index))),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -37,5 +75,20 @@ class TeamPage extends StatelessWidget {
         child: Icon(Icons.person_add),
       ),
     );
+  }
+}
+
+class Debouncer {
+  final int milliseconds;
+  VoidCallback action;
+  Timer _timer;
+
+  Debouncer({this.milliseconds});
+
+  run(VoidCallback action) {
+    if (_timer != null) {
+      _timer.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
   }
 }
