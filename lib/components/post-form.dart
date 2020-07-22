3b2ceec40a2afd1247503c863eee0488c92a0825
Vinícius Mioto <60669449+viniciusmioto@users.noreply.github.com>
2020-07-22 18:35:26 -0300

    post-form

--- lib/components/post-form.dart
+++ lib/components/post-form.dart
@@ -27,77 +27,74 @@ class _PostFormState extends State<PostForm> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             UserData userData = snapshot.data;
-            return Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 300.0),
-              child: Scaffold(
-                appBar: AppBar(
-                  title: Text('Nova Postagem'),
-                  centerTitle: true,
-                  actions: [
-                    IconButton(
-                      icon: Icon(Icons.publish),
-                      onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          setState(() => loading = true);
-                          dynamic result =
-                              await DatabaseService(uid: user.uid).createPost(
-                            text,
-                            userData.avatarUrl ??
-                                'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png',
-                            userData.nome,
-                            user.uid,
-                            worldTime.postDateTime,
-                          );
-                          if (result == null) {
-                            setState(() => error = 'Erro ao publicar...');
-                          }
-                          Navigator.pop(context);
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Nova Postagem'),
+                centerTitle: true,
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.publish),
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        setState(() => loading = true);
+                        dynamic result =
+                            await DatabaseService(uid: user.uid).createPost(
+                          text,
+                          userData.avatarUrl ??
+                              'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png',
+                          userData.nome,
+                          user.uid,
+                          worldTime.postDateTime,
+                        );
+                        if (result == null) {
+                          setState(() => error = 'Erro ao publicar...');
                         }
-                      },
-                    )
-                  ],
-                ),
-                body: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ListView(
-                    children: [
-                      Form(
-                        key: _formKey,
-                        child: Column(
-                          children: <Widget>[
-                            SizedBox(height: 10),
-                            Container(
-                              height: 120,
-                              child: TextFormField(
-                                expands: true,
-                                minLines: null,
-                                maxLines: null,
-                                autofocus: true,
-                                onChanged: (value) {
-                                  setState(() => text = value);
-                                },
-                                validator: (value) =>
-                                    value.isEmpty ? 'Insira um texto' : null,
-                                keyboardType: TextInputType.text,
-                                decoration: InputDecoration(
-                                  labelText: "Mensagem",
-                                  labelStyle: TextStyle(
-                                    color: Colors.black45,
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                style: TextStyle(
+                        Navigator.pop(context);
+                      }
+                    },
+                  )
+                ],
+              ),
+              body: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ListView(
+                  children: [
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 10),
+                          Container(
+                            height: 120,
+                            child: TextFormField(
+                              expands: true,
+                              minLines: null,
+                              maxLines: null,
+                              autofocus: true,
+                              onChanged: (value) {
+                                setState(() => text = value);
+                              },
+                              validator: (value) =>
+                                  value.isEmpty ? 'Insira um texto' : null,
+                              keyboardType: TextInputType.text,
+                              decoration: InputDecoration(
+                                labelText: "Mensagem",
+                                labelStyle: TextStyle(
+                                  color: Colors.black45,
+                                  fontWeight: FontWeight.w400,
                                   fontSize: 18,
                                 ),
                               ),
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      Text(error),
-                    ],
-                  ),
+                    ),
+                    Text(error),
+                  ],
                 ),
               ),
             );
